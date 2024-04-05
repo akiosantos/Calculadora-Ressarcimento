a881da503b3d46987e75499e9821ebf2e3a58598
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-04 21:20:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 function calcularRessarcimento() {
     var faturamentoTotalInput = document.getElementById("faturamento-total");
     var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace(/[^\d.,]/g, ''); // Remover todos os caracteres, exceto números, pontos e vírgulas
+    var faturamentoTotal = parseFloat(faturamentoTotalValue.replace(',', '.')); // Substituir vírgulas por pontos e converter para número
 
     if (isNaN(faturamentoTotal)) {
         alert("Por favor, insira um valor válido para o Faturamento Total.");
