26a4ae14a3ee07fa77d03398d7c6822026e9b8ad
Lucas Akio <107128189+akiosantos@users.noreply.github.com>
2024-04-04 21:21:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
+// Função para formatar um número como moeda
+function formatarMoeda(valor) {
+    return valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+// Função para formatar o valor do faturamento total
+function formatarFaturamentoTotal() {
+    var faturamentoTotalInput = document.getElementById("faturamento-total");
+    var valor = faturamentoTotalInput.value.trim();
+  
+    // Remover todos os caracteres, exceto números, pontos e vírgulas
+    valor = valor.replace(/[^0-9,.]/g, '');
+  
+    // Verificar se o valor é válido
+    if (valor !== '') {
+        // Converter para número
+        var numero = parseFloat(valor.replace(',', '.')); // Substituir vírgulas por pontos
+
+        // Verificar se o número é válido
+        if (!isNaN(numero)) {
+            // Formatar o número como moeda
+            var valorFormatado = formatarMoeda(numero);
+
+            // Definir o valor do input como moeda formatada
+            faturamentoTotalInput.value = valorFormatado;
+
+            // Calcular o ressarcimento após a formatação do valor do faturamento total
+            calcularRessarcimento();
+        } else {
+            alert('Por favor, insira um valor numérico válido.');
+            faturamentoTotalInput.value = ''; // Limpar o campo se o valor não for válido
+        }
+    }
+}
+
 // Função para calcular o ressarcimento
 function calcularRessarcimento() {
     var faturamentoTotalInput = document.getElementById("faturamento-total");
-    var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace(/[^\d.,]/g, ''); // Remover todos os caracteres, exceto números, pontos e vírgulas
+    var faturamentoTotalValue = faturamentoTotalInput.value.trim().replace(/[^0-9,.]/g, ''); // Remover todos os caracteres, exceto números, pontos e vírgulas
     var faturamentoTotal = parseFloat(faturamentoTotalValue.replace(',', '.')); // Substituir vírgulas por pontos e converter para número
 
     if (isNaN(faturamentoTotal)) {
@@ -11,8 +46,7 @@ function calcularRessarcimento() {
 
     var meses = parseInt(document.getElementById("meses").value);
 
-    // Calcular a base de cálculo corretamente (1,2% do faturamento total)
-    var baseCalculo = faturamentoTotal * 0.012; // 12% do faturamento total
+    var baseCalculo = faturamentoTotal * 0.012; // Calcula a base de cálculo corretamente
 
     // Formatar a base de cálculo com vírgula a cada 3 dígitos
     var baseCalculoFormatado = formatarMoeda(baseCalculo);
